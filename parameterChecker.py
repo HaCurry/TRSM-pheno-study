92e8462b31fa74bdbfe3182196040b3236f93705
ihaque <iram.haque38@gmail.com>
2023-11-27 14:09:43 +0100

    State before starting work on parameterPlotter. Planning to create a JSON file when param executes in parameterData.py storing info, especially needed for checking exclusion plots.

---
+++ parameterChecker.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import csv
+import pandas
+
+import numpy as np
+from scipy.interpolate import CubicSpline
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import scipy.interpolate
+mpl.rcParams.update(mpl.rcParamsDefault)
+
+import subprocess
+import configparser
+import os
+import datetime
+import multiprocessing
+import sys
+import glob
+
+targetDir = 'test6'
+test = os.listdir(targetDir)
+# print(test)
+# for i in test:
+    # print(i)
+
+# test2 = os.listdir(targetDir + '/' + test[0])
+
+test2 = glob.glob(targetDir + '/**/output_*.tsv', recursive = True)
+test3 = glob.glob(targetDir + '/**/config_*.ini', recursive = True)
+
+for i in test2:
+    print(i)
+
+print('===============================')
+
+for i in test3:
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
