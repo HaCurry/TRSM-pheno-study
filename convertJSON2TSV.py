884c552240615efb66a7755dba440fbf47b9d92f
ihaque <iram.haque38@gmail.com>
2023-12-05 18:27:32 +0100

    Made everything in convertJSON2TSV.py to functions for future use (other BPs and their mass ranges)

--- convertJSON2TSV.py
+++ convertJSON2TSV.py
@@ -3,37 +3,12 @@ import json
 import pandas
 import numpy as np
 
-limits = pandas.read_json('Atlas2023Limits.json')
-
-mx, ms, limit_obs, limit_exp = [], [], [], []
-
-for element in limits:
-    mx.append((limits[element])[0])
-    ms.append((limits[element])[1])
-#    limit_exp.append((limits[element])[2] * 10 **(-3))
-#    limit_obs.append((limits[element])[3] * 10 **(-3))
-    # limit_exp.append((limits[element])[2] * 10 **(-3))
-    # limit_obs.append((limits[element])[3] * 10 **(-3))
-    limit_exp.append((limits[element])[2] )
-    limit_obs.append((limits[element])[3] )
-
-mx = np.array(mx)
-ms = np.array(ms)
-limit_exp = np.array(limit_exp)
-limit_obs = np.array(limit_obs)
-
-d = {'ms': ms, 'mx': mx, 'limit_obs': limit_obs, 'limit_exp': limit_exp}
-
-df = pandas.DataFrame(data = d)
-
-print(df)
-print('===============')
-print(df.dtypes)
-
-df.to_csv('Atlas2023Limits.tsv', sep = "\t")
+# !!IMPORTANT: ALL VALUES FROM Atlas2023Limits.json ARE IN FB SO ARE ALSO THE CONVERTED .tsv FILES!!
 
 
-def constrained_observed_lim(ms, mx, limit_obs, ms_lb = 1, ms_ub = 124, mx_lb = 126, mx_ub = 500, LessThanOrEqualTo = True):
+
+def constrained_observed_lim(ms, mx, limit_obs, ms_lb, ms_ub, mx_lb, mx_ub, LessThanOrEqualTo = True):
+
     ms_BP2constrained = []
     mx_BP2constrained = []
     limit_obs_BP2constrained = []
@@ -65,31 +40,65 @@ def constrained_observed_lim(ms, mx, limit_obs, ms_lb = 1, ms_ub = 124, mx_lb = 
         
         return ms_BP2constrained, mx_BP2constrained, limit_obs_BP2constrained
 
-ms_BP2constrained, mx_BP2constrained, limit_obs_BP2constrained = constrained_observed_lim(ms, mx, limit_obs)
-
-dBP2 = {'ms_BP2': ms_BP2constrained, 'mx_BP2': mx_BP2constrained, 'limit_obs_BP2': limit_obs_BP2constrained}
-
-dfBP2 = pandas.DataFrame(data = dBP2)
-
-print(dfBP2)
-print('===================')
-print(df.dtypes)
-
-dfBP2.to_csv('Atlas2023Limits_BP2lessThanOrEqualTo.tsv', sep = "\t")
 
 
+def convert(path, output, constraint, constraintLessThanOrEqualTo = True, **kwargs):
 
+    # BP2 mass constraints ms_lb = 1, ms_ub = 124, mx_lb = 126, mx_ub = 500
+    
+    #################### kwargs ####################
 
+    if constraint == True:
 
+        if ('ms_lb' in kwargs) and ('ms_ub' in kwargs) and ('mx_lb' in kwargs) and ('mx_ub' in kwargs):
+            ms_lb = kwargs['ms_lb']
+            ms_ub = kwargs['ms_ub']
+            mx_lb = kwargs['mx_lb']
+            mx_ub = kwargs['mx_ub']
 
+        else:
+            raise Exception('constraint set to true but no constraint values ms_lb, ms_ub, mx_lb, mx_ub given \n or one value is missing.')
 
+    else:
+        constraint = False
+                             
+     ################################################
 
+    
+    limits = pandas.read_json(path)
+    
+    mx, ms, limit_obs, limit_exp = [], [], [], []
+    
+    for element in limits:
+        mx.append((limits[element])[0])
+        ms.append((limits[element])[1])
+        limit_exp.append((limits[element])[2] )
+        limit_obs.append((limits[element])[3] )
+    
+    mx = np.array(mx)
+    ms = np.array(ms)
+    limit_exp = np.array(limit_exp)
+    limit_obs = np.array(limit_obs)
 
+    if constraint == True:
+        ms, mx, limit_obs = constrained_observed_lim(ms, mx, limit_obs, ms_lb, ms_ub, mx_lb, mx_ub, LessThanOrEqualTo = constraintLessThanOrEqualTo)
 
+    else:
+        pass
+    
+    
+    d = {'ms': ms, 'mx': mx, 'limit_obs': limit_obs}
+    
+    df = pandas.DataFrame(data = d)
 
+    df.to_csv(output, sep = "\t")
 
+    return df
+    
+if __name__ == "__main__":
 
+    # BP2 mass points in Atlas2023Limits
+    dfBP2 = convert('Atlas2023Limits.json', 'Atlas2023Limits_BP2lessThanOrEqualToTrue.tsv', True, constraintLessThanOrEqualTo = True, ms_lb = 1, ms_ub = 124, mx_lb = 126, mx_ub = 500)
 
-
-
-
+    # BP3 mass points in Atlas2023Limits
+    dfBP3 = convert('Atlas2023Limits.json', 'Atlas2023Limits_BP3lessThanOrEqualToTrue.tsv', True, constraintLessThanOrEqualTo = True, ms_lb = 126, ms_ub = 500, mx_lb = 255, mx_ub = 650)
