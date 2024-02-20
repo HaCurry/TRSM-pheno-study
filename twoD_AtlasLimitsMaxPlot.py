the scripts plotting maximum cross sections are done in twoD_AtlasLimitsMaxPlots.py, found also a bug where I forgot a factor of 10**(-3). What remains is to compare with the limits and the BP values and making sure the scripts are thouroghly tested.

twoD_AtlasLimitsMaxPlot.py
@@ -1,5 +1,5 @@
 import pandas
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 import matplotlib.patheffects
@@ -83,17 +83,21 @@ if __name__ == '__main__':
                             max = {x_H1_gamgam_H2_bb}\n\
                             ObsLim = {ObsLim_H1_gamgam_H2_bb}')
 
-
     print(f'# of elements: ms: {len(ms)}, mx: {len(mx)}, max: {len(max)}, ObsLim: {len(ObsLim)}')
 
-    print('testing')
+    ms = np.array(ms)
+    mx = np.array(mx)
+    max = np.array(max)
+    ObsLim = np.array(ObsLim)
+
+    print(ms[0:3], mx[0:3], max[0:3], ObsLim[0:3])
 
     plt.style.use(hep.style.ATLAS)
     hep.style.use({"mathtext.default": "rm"})
     matplotlib.rcParams['axes.labelsize'] = 19
     matplotlib.rcParams['axes.titlesize'] = 19
 
-    norm = (31.02 * 0.0026)
+    norm = (31.02 * 0.0026) * 10**(-3)
 
     ## Low mass
 
@@ -109,7 +113,7 @@ if __name__ == '__main__':
 
     for i in range(len(max/norm)):
         if ms[i] < 85 or ms[i] > 115 or mx[i] < 210 or mx[i] > 260:
-            ax.annotate('{:.1f}'.format(max/norm[i]), (ms[i], mx[i]),
+            ax.annotate('{:.3f}'.format(max[i]/norm), (ms[i], mx[i]),
                          textcoords='offset points', xytext=(-3,-2), fontsize=10, rotation=45, 
                          path_effects=[matplotlib.patheffects.withStroke(linewidth=1.5, foreground='w')])
         else: continue
@@ -136,7 +140,7 @@ if __name__ == '__main__':
     mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
 
     for i in range(len(max/norm)):
-        axins.annotate('{:.1f}'.format(max/norm[i]), (ms[i], mx[i]),
+        axins.annotate('{:.3f}'.format(max[i]/norm), (ms[i], mx[i]),
                      textcoords='offset points', xytext=(-3,-2), fontsize=10, rotation=45, 
                      path_effects=[matplotlib.patheffects.withStroke(linewidth=1.5, foreground='w')])
 
@@ -145,14 +149,15 @@ if __name__ == '__main__':
 
     ax.set_xlabel(r'$M_{S}$ [GeV]')
     ax.set_ylabel(r'$M_{X}$ [GeV]')
-    ax.set_title(r'BP-independent cross sections $\sigma(S(b\bar{b})H(\gamma\gamma))/\sigma(SM)$, small $M_{X}$')
+    ax.set_title(r'$\sigma(S(b\bar{b})H(\gamma\gamma))/\sigma(SM)$, small $M_{X}$')
 
     fig = plt.gcf()
     fig.colorbar(im, ax=ax, label =r'$\sigma(S(b\bar{b})H(\gamma\gamma))/\sigma(SM)$' )
 
     plt.tight_layout()
-    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/norm/AtlasLimitsMax/norm_configure3/plots/AtlasLimitsMax_lowmass.png", format='png')
-    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/norm/AtlasLimitsMax/norm_configure3/plots/AtlasLimitsMax_lowmass.pdf")
+    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/AtlasLimitsMax_configure3/plots/AtlasLimitsMax_lowmass.png", format='png')
+    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/AtlasLimitsMax_configure3/plots/AtlasLimitsMax_lowmass.pdf")
+    plt.close()
 
     # DO NOT TURN ON SHOW, THE ZOOM BOX IS PLACE INCORRECTLY WHEN USING THE MATPLOTLIB WINDOW PANE OR SAVING IT AS A PNG  #  plt.close()
 
@@ -161,7 +166,7 @@ if __name__ == '__main__':
 
     plt.scatter(ms, mx, c=max/norm)
     for i in range(len(max/norm)):
-        axins.annotate('{:.1f}'.format(max/norm[i]), (ms[i], mx[i]),
+        plt.annotate('{:.3f}'.format(max[i]/norm), (ms[i], mx[i]),
                      textcoords='offset points', xytext=(-3,-2), fontsize=10, rotation=45, 
                      path_effects=[matplotlib.patheffects.withStroke(linewidth=1.5, foreground='w')])
     # msAnnotate, mxAnnotate, max_divNormAnnotate = cutter(ms, mx, max/norm, (0, 585), (420, 1020))
@@ -173,14 +178,14 @@ if __name__ == '__main__':
     plt.xlabel(r'$M_{S}$ [GeV]')
     plt.ylabel(r'$M_{X}$ [GeV]')
 
-    plt.title(r'BP-independent cross sections $\sigma(S(b\bar{b})H(\gamma\gamma))/\sigma(SM)$, medium $M_{X}$')
+    plt.title(r'$\sigma(S(b\bar{b})H(\gamma\gamma))/\sigma(SM)$, medium $M_{X}$')
 
     # plt.colorbar(label =r'$\sigma_{ gg \ \rightarrow \ h_{X}} \cdot \mathrm{BR}_{h_{X} \ \to \ h_{S}(b\bar{b}) \ h_{H}(\gamma\gamma) } \ / \ \sigma_{gg \ \to \ h_{\mathrm{SM}} \ \to \ b\bar{b}\gamma\gamma }$' )
     plt.colorbar(label =r'$\sigma(S(b\bar{b})H(\gamma\gamma))/\sigma(SM)$' )
 
     plt.tight_layout()
-    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/norm/AtlasLimitsMax/norm_configure3/plots/AtlasLimitsMax_mediummass.png", format='png')
-    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/norm/AtlasLimitsMax/norm_configure3/plots/AtlasLimitsMax_mediummass.pdf")
+    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/AtlasLimitsMax_configure3/plots/AtlasLimitsMax_mediummass.png", format='png')
+    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/AtlasLimitsMax_configure3/plots/AtlasLimitsMax_mediummass.pdf")
 
     # plt.show()
     plt.close()
@@ -190,7 +195,7 @@ if __name__ == '__main__':
 
     plt.scatter(ms, mx, c=max/norm)
     for i in range(len(max/norm)):
-        axins.annotate('{:.1f}'.format(max/norm[i]), (ms[i], mx[i]),
+        plt.annotate('{:.3f}'.format(max[i]/norm), (ms[i], mx[i]),
                      textcoords='offset points', xytext=(-3,-2), fontsize=10, rotation=45, 
                      path_effects=[matplotlib.patheffects.withStroke(linewidth=1.5, foreground='w')])
     # msAnnotate, mxAnnotate, max_divNormAnnotate = cutter(ms, mx, max/norm, (0, 585), (420, 1020))
@@ -202,15 +207,15 @@ if __name__ == '__main__':
     plt.xlabel(r'$M_{S}$ [GeV]')
     plt.ylabel(r'$M_{X}$ [GeV]')
 
-    plt.title(r'BP-independent cross sections $\sigma(S(b\bar{b})H(\gamma\gamma))/\sigma(SM)$, large $M_{X}$')
+    plt.title(r'$\sigma(S(b\bar{b})H(\gamma\gamma))/\sigma(SM)$, large $M_{X}$')
 
     # plt.colorbar(label =r'$\sigma_{ gg \ \rightarrow \ h_{X}} \cdot \mathrm{BR}_{h_{X} \ \to \ h_{S}(b\bar{b}) \ h_{H}(\gamma\gamma) } \ / \ \sigma_{gg \ \to \ h_{\mathrm{SM}} \ \to \ b\bar{b}\gamma\gamma }$' )
     plt.colorbar(label =r'$\sigma(S(b\bar{b})H(\gamma\gamma))/\sigma(SM)$' )
 
 
     plt.tight_layout()
-    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/norm/AtlasLimitsMax/norm_configure3/plots/AtlasLimitsMax_largemass.png", format='png')
-    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/norm/AtlasLimitsMax/norm_configure3/plots/AtlasLimitsMax_largemass.pdf")
+    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/AtlasLimitsMax_configure3/plots/AtlasLimitsMax_largemass.png", format='png')
+    plt.savefig("/eos/user/i/ihaque/AtlasLimitsMax/AtlasLimitsMax_configure3/plots/AtlasLimitsMax_largemass.pdf")
 
     # plt.show()
     plt.close()
@@ -234,6 +239,6 @@ if __name__ == '__main__':
 
     
     plt.scatter(ms, mx, c=ObsLim)
-    plt.savefig('/eos/user/i/ihaque/AtlasLimitsMax/norm/AtlasLimitsMax/norm_configure3/plots/scatter')
+    plt.savefig('/eos/user/i/ihaque/AtlasLimitsMax/AtlasLimitsMax_configure3/plots/scatter')
     plt.close()
 