ed00a5c78f531fee7a9f53c8edfa442f1b99fb81
Irtiza Hayder Iram Haque <ihaque@lxplus914.cern.ch>
2024-04-30 14:08:43 +0200

    Removed some lines which may not be important in the madgraph script twoD_mgCrossSections.py generates.

--- testing/MadGraph_ResonVsNonReson/twosinglet_scalarcouplings/twoD_mgCrossSections.py
+++ testing/MadGraph_ResonVsNonReson/twosinglet_scalarcouplings/twoD_mgCrossSections.py
@@ -655,20 +655,6 @@ def generateScript(pathOutput, pathMadgraphOutput, pathRun_card, pathLoop_sm_two
 #*     run as ./bin/mg5_aMC  filename                       *
 #*                                                          *
 #************************************************************
-set group_subprocesses Auto
-set ignore_six_quark_processes False
-set low_mem_multicore_nlo_generation False
-set complex_mass_scheme False
-set include_lepton_initiated_processes False
-set gauge unitary
-set loop_optimized_output True
-set loop_color_flows False
-set max_npoint_for_channel 0
-set default_unset_couplings 99
-set max_t_for_channel 99
-set zerowidth_tchannel True
-set nlo_mixed_expansion True
-import model sm
 define p = g u c d s u~ c~ d~ s~
 define j = g u c d s u~ c~ d~ s~
 define l+ = e+ mu+
