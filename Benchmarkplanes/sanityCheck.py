175ea1225b148bac59a153059634af7b8baf0c41
Irtiza Hayder Iram Haque <ihaque@lxplus972.cern.ch>
2024-05-07 18:42:40 +0200

    Updated sanityCheck.py

--- Benchmarkplanes/sanityCheck.py
+++ Benchmarkplanes/sanityCheck.py
@@ -9,82 +9,43 @@ from helpScannerS import functions as TRSM
 # E:
 pathRepo = '/afs/cern.ch/user/i/ihaque/scannerS/ScannerS-master/build/sh-bbyy-pheno'
 
-# path to ScannerS TRSM executable
-path13_BP2 = os.path.join(pathRepo, 'Benchmarkplanes/BPs_noconstraints/BP2/output_BP2_noconstraints.tsv')
-path13_BP3 = os.path.join(pathRepo, 'Benchmarkplanes/BPs_noconstraints/BP3/output_BP3_noconstraints.tsv')
+# path to ScannerS TRSM executable output
+pathBP = os.path.join(pathRepo, 'Benchmarkplanes/BPs_noconstraints/')
 
-## BP2
+for BPX in ['BP2', 'BP3']: 
+    path = os.path.join(pathBP, BPX, f'output_{BPX}_noconstraints.tsv')
+    normSM = 1
+    BPX_13 = TRSM.observables(path, 'bb', 'gamgam', 'mH1', 'mH2', 'mH3', normSM=normSM)
 
-BP2_13 = TRSM.observables(path13_BP2, 'bb', 'gamgam', 'mH1', 'mH2', 'mH3', normSM=1)
+    row = -1
 
-row = -1
+    print(f'\nDoing {BPX}:')
 
-print('\nDoing BP2:')
+    print('\ncalculated with TRSM.observables')
 
-print('\ncalculated with TRSM.observables')
+    print(f'mH1: {BPX_13["mH1"][row]}')
+    print(f'mH2: {BPX_13["mH2"][row]}')
+    print(f'mH3: {BPX_13["mH3"][row]}')
+    BPXbbgamgam = BPX_13["x_H3_H1_bb_H2_gamgam"][row]
+    BPXgamgambb = BPX_13["x_H3_H1_gamgam_H2_bb"][row]
+    print(f'\sigma(H3 -> H1(bb) H2(gamgam)): {BPXbbgamgam}')
+    print(f'\sigma(H3 -> H1(gamgam) H2(bb)): {BPXgamgambb}')
 
-print(f'mH1: {BP2_13["mH1"][row]}')
-print(f'mH2: {BP2_13["mH2"][row]}')
-print(f'mH3: {BP2_13["mH3"][row]}')
-BP2bbgamgam = BP2_13["x_H3_H1_bb_H2_gamgam"][row]
-BP2gamgambb = BP2_13["x_H3_H1_gamgam_H2_bb"][row]
-print(f'\sigma(H3 -> H1(bb) H2(gamgam)): {BP2bbgamgam}')
-print(f'\sigma(H3 -> H1(gamgam) H2(bb)): {BP2gamgambb}')
+    print('\ncalculated by hand')
 
-print('\ncalculated by hand')
+    dfBPX = pandas.read_table(path)
+    print(f'mH1: {np.array(dfBPX["mH1"])[row]}')
+    print(f'mH2: {np.array(dfBPX["mH2"])[row]}')
+    print(f'mH3: {np.array(dfBPX["mH3"])[row]}')
 
-dfBP2 = pandas.read_table(path13_BP2)
-print(f'mH1: {np.array(dfBP2["mH1"])[row]}')
-print(f'mH2: {np.array(dfBP2["mH2"])[row]}')
-print(f'mH3: {np.array(dfBP2["mH3"])[row]}')
+    BPXbbgamgam_byhand = np.array(dfBPX["x_H3_gg"])[row] * np.array(dfBPX["b_H3_H1H2"])[row] * np.array(dfBPX["b_H1_bb"])[row] * np.array(dfBPX["b_H2_gamgam"])[row]/normSM
+    BPXgamgambb_byhand = np.array(dfBPX["x_H3_gg"])[row] * np.array(dfBPX["b_H3_H1H2"])[row] * np.array(dfBPX["b_H1_gamgam"])[row] * np.array(dfBPX["b_H2_bb"])[row]/normSM
+    print(f'x_H3_gg * b_H3_H1H2 * b_H1_bb * b_H2_gamgam: {BPXbbgamgam_byhand}')
+    print(f'x_H3_gg * b_H3_H1H2 * b_H1_gamgam * b_H2_bb: {BPXgamgambb_byhand}')
 
-BP2bbgamgam_byhand = np.array(dfBP2["x_H3_gg"])[row] * np.array(dfBP2["b_H3_H1H2"])[row] * np.array(dfBP2["b_H1_bb"])[row] * np.array(dfBP2["b_H2_gamgam"])[row]
-BP2gamgambb_byhand = np.array(dfBP2["x_H3_gg"])[row] * np.array(dfBP2["b_H3_H1H2"])[row] * np.array(dfBP2["b_H1_gamgam"])[row] * np.array(dfBP2["b_H2_bb"])[row]
-print(f'x_H3_gg * b_H3_H1H2 * b_H1_bb * b_H2_gamgam: {BP2bbgamgam_byhand}')
-print(f'x_H3_gg * b_H3_H1H2 * b_H1_gamgam * b_H2_bb: {BP2gamgambb_byhand}')
+    print('\nchecking the difference:')
+    print(f'\sigma(H3 -> H1(bb) H2(gamgam)): {BPXbbgamgam - BPXbbgamgam_byhand}')
+    print(f'\sigma(H3 -> H1(gamgam) H2(bb)): {BPXgamgambb - BPXgamgambb_byhand}')
 
-print('\nchecking the difference:')
-print(f'\sigma(H3 -> H1(bb) H2(gamgam)): {BP2bbgamgam - BP2bbgamgam_byhand}')
-print(f'\sigma(H3 -> H1(gamgam) H2(bb)): {BP2gamgambb - BP2gamgambb_byhand}')
+    print('==========================================')
 
-print('==========================================')
-
-## BP3
-
-BP3_13 = TRSM.observables(path13_BP3, 'bb', 'gamgam', 'mH1', 'mH2', 'mH3', normSM=1)
-
-row = -1
-
-print('\nDoing BP3:')
-
-print('\ncalculated with TRSM.observables')
-
-print(f'mH1: {BP3_13["mH1"][row]}')
-print(f'mH2: {BP3_13["mH2"][row]}')
-print(f'mH3: {BP3_13["mH3"][row]}')
-BP3bbgamgam = BP3_13["x_H3_H1_bb_H2_gamgam"][row]
-BP3gamgambb = BP3_13["x_H3_H1_gamgam_H2_bb"][row]
-print(f'\sigma(H3 -> H1(bb) H2(gamgam)): {BP3bbgamgam}')
-print(f'\sigma(H3 -> H1(gamgam) H2(bb)): {BP3gamgambb}')
-
-print('\ncalculated by hand')
-
-dfBP3 = pandas.read_table(path13_BP3)
-print(f'mH1: {np.array(dfBP3["mH1"])[row]}')
-print(f'mH2: {np.array(dfBP3["mH2"])[row]}')
-print(f'mH3: {np.array(dfBP3["mH3"])[row]}')
-
-BP3bbgamgam_byhand = np.array(dfBP3["x_H3_gg"])[row] * np.array(dfBP3["b_H3_H1H2"])[row] * np.array(dfBP3["b_H1_bb"])[row] * np.array(dfBP3["b_H2_gamgam"])[row]
-BP3gamgambb_byhand = np.array(dfBP3["x_H3_gg"])[row] * np.array(dfBP3["b_H3_H1H2"])[row] * np.array(dfBP3["b_H1_gamgam"])[row] * np.array(dfBP3["b_H2_bb"])[row]
-print(f'x_H3_gg * b_H3_H1H2 * b_H1_bb * b_H2_gamgam: {BP3bbgamgam_byhand}')
-print(f'x_H3_gg * b_H3_H1H2 * b_H1_gamgam * b_H2_bb: {BP3gamgambb_byhand}')
-
-print('\nchecking the difference:')
-print(f'\sigma(H3 -> H1(bb) H2(gamgam)): {BP3bbgamgam - BP3bbgamgam_byhand}')
-print(f'\sigma(H3 -> H1(gamgam) H2(bb)): {BP3gamgambb - BP3gamgambb_byhand}')
-
-print('==========================================')
-
-print('\n\nCalculating at higher \sqrt{s} energies:')
-
-print('\n')
