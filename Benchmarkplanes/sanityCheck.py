5539085c832bbc068351ae1dfe3b5628646ab8c1
Irtiza Hayder Iram Haque <ihaque@lxplus972.cern.ch>
2024-05-07 19:42:51 +0200

    sanityCheck script pretty much complete

--- Benchmarkplanes/sanityCheck.py
+++ Benchmarkplanes/sanityCheck.py
@@ -49,3 +49,63 @@ for BPX in ['BP2', 'BP3']:
 
     print('==========================================')
 
+print('Doing beyond 13 TeV')
+
+path13_6 = os.path.join(pathRepo, 'testing/SusHi_HiggsCrossSections',
+                        '13_6TeV_SusHiImprCrossSections.tsv')
+path14 = os.path.join(pathRepo, 'testing/SusHi_HiggsCrossSections',
+                      '14TeV_YR4CrossSections.tsv')
+
+# E:
+beyond13dict = {13.6: {'path': path13_6, 'BP2_x_HSM_gg': 5.049227246917398,
+                                         'BP3_x_HSM_gg': 1.5207048620695935},
+                14:   {'path': path14,   'BP2_x_HSM_gg': 5.401E+00,
+                                         'BP3_x_HSM_gg': 1.636E+00}}
+
+# E:
+BPXdict = {'BP2': {'row':-1 },
+           'BP3': {'row':-1}}
+
+for keyBeyond in beyond13dict:
+
+    print(f'Doing {keyBeyond} TeV')
+
+    for keyBPX in BPXdict: 
+        path = os.path.join(pathBP, keyBPX, f'output_{keyBPX}_noconstraints.tsv')
+        normSM = 1
+        BPX_13 = TRSM.observables(path, 'bb', 'gamgam', 'mH1', 'mH2', 'mH3', normSM=normSM,
+                                  kineticExclude=True,
+                                  pathRun3Data=beyond13dict[keyBeyond]['path'],
+                                  keyCrossSecRun3='SMCrossSec')
+
+        row = -1
+
+        print(f'\nDoing {keyBPX}:')
+
+        print(f'\ncalculated with TRSM.observables at {keyBeyond} TeV')
+
+        print(f'mH1: {BPX_13["mH1"][BPXdict[keyBPX]["row"]]}')
+        print(f'mH2: {BPX_13["mH2"][BPXdict[keyBPX]["row"]]}')
+        print(f'mH3: {BPX_13["mH3"][BPXdict[keyBPX]["row"]]}')
+        BPXbbgamgam = BPX_13["x_H3_H1_bb_H2_gamgam"][BPXdict[keyBPX]["row"]]
+        BPXgamgambb = BPX_13["x_H3_H1_gamgam_H2_bb"][BPXdict[keyBPX]["row"]]
+        print(f'\sigma(H3 -> H1(bb) H2(gamgam)): {BPXbbgamgam}')
+        print(f'\sigma(H3 -> H1(gamgam) H2(bb)): {BPXgamgambb}')
+
+        print(f'\ncalculated by hand at {keyBeyond} TeV')
+
+        dfBPX = pandas.read_table(path)
+        print(f'mH1: {np.array(dfBPX["mH1"])[BPXdict[keyBPX]["row"]]}')
+        print(f'mH2: {np.array(dfBPX["mH2"])[BPXdict[keyBPX]["row"]]}')
+        print(f'mH3: {np.array(dfBPX["mH3"])[BPXdict[keyBPX]["row"]]}')
+
+        BPXbbgamgam_byhand = (np.array(dfBPX["R31"])[BPXdict[keyBPX]["row"]])**2 * beyond13dict[keyBeyond][f'{keyBPX}_x_HSM_gg'] * np.array(dfBPX["b_H3_H1H2"])[BPXdict[keyBPX]["row"]] * np.array(dfBPX["b_H1_bb"])[BPXdict[keyBPX]["row"]] * np.array(dfBPX["b_H2_gamgam"])[BPXdict[keyBPX]["row"]]/(normSM)
+        BPXgamgambb_byhand = (np.array(dfBPX["R31"])[BPXdict[keyBPX]["row"]])**2 * beyond13dict[keyBeyond][f'{keyBPX}_x_HSM_gg'] * np.array(dfBPX["b_H3_H1H2"])[BPXdict[keyBPX]["row"]] * np.array(dfBPX["b_H1_gamgam"])[BPXdict[keyBPX]["row"]] * np.array(dfBPX["b_H2_bb"])[BPXdict[keyBPX]["row"]]/(normSM)
+        print(f'x_H3_gg * b_H3_H1H2 * b_H1_bb * b_H2_gamgam: {BPXbbgamgam_byhand}')
+        print(f'x_H3_gg * b_H3_H1H2 * b_H1_gamgam * b_H2_bb: {BPXgamgambb_byhand}')
+
+        print('\nchecking the difference:')
+        print(f'\sigma(H3 -> H1(bb) H2(gamgam)): {BPXbbgamgam - BPXbbgamgam_byhand}')
+        print(f'\sigma(H3 -> H1(gamgam) H2(bb)): {BPXgamgambb - BPXgamgambb_byhand}')
+
+        print('==========================================')
