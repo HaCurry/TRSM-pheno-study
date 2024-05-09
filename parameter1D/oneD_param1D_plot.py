64181922454c5bd8abf2318879410f29c3bf831a
Irtiza Hayder Iram Haque <ihaque@lxplus903.cern.ch>
2024-05-09 18:23:48 +0200

    half way finishing the plotting scripts in parameter1D specifically oneD_param1D_plot.py

--- parameter1D/oneD_param1D_plot.py
+++ parameter1D/oneD_param1D_plot.py
@@ -11,44 +11,45 @@ if __name__ == '__main__':
 
     ## paths
 
+    # path to repo
+    # E:
+    pathRepo = '/afs/cern.ch/user/i/ihaque/scannerS/ScannerS-master/build/sh-bbyy-pheno'
+
     # path to condor job output
     # E:
     pathOutputParent = '/eos/user/i/ihaque/parameter1DPlots' 
     
-    ## BP2
+    # path to plots
+    pathPlots = os.path.join(pathRepo, 'plots')
+    
+    # angles
 
-    # region 1
+    for BPX in ['BP2', 'BP3']:
 
+        for regionX in ['region1', 'region2']:
 
-    # for path in TRSMOutput_BP2_R1_paths:
-    #     df = pandas.read_table(path)
-    #     obs = TRSM.observables(path, 'bb', 'gamgam', 'thetahS')
-    #     plt.plot(obs['thetahS'], obs['x_H3_H1_gamgam_H2_bb'])
+            for angle in ['thetahS', 'thetahX', 'thetaSX']:
 
-    plt.savefig('test2.pdf')
-    plt.close()
+                for process in ['x_H3_H1_bb_H2_gamgam', 'x_H3_H1_gamgam_H2_bb']:
+    
+                    TRSMOutput_BPX_regionX_paths = glob.glob(os.path.join(pathOutputParent, BPX, regionX, 'X*S*'),
+                                                             recursive=True)
 
-    # region 2
+                    fig, ax = plt.subplots()
 
-    TRSMOutput_BP2_R2_paths = glob.glob(os.path.join(pathOutputParent, 'BP2', 'region2', 'X*S*'),
-                                                recursive=True)
-
-    for path in TRSMOutput_BP2_R2_paths:
-        print(path)
-        pathNofree = glob.glob(os.path.join(path, 'nofree', 'output*'))[0]
-        obs_nofree = TRSM.observables(pathNofree, 'bb', 'gamgam')
+                    for path in TRSMOutput_BPX_regionX_paths:
+                        print(path)
+                        pathNofree = glob.glob(os.path.join(path, 'nofree', 'output*'))[0]
+                        obs_nofree = TRSM.observables(pathNofree, 'bb', 'gamgam')
         
-        free = 'thetahS'
-        pathFree = glob.glob(os.path.join(path, free, 'output_*'))[0]
-        obs = TRSM.observables(pathFree, 'bb', 'gamgam', 'thetahS')
-        process = 'x_H3_H1_bb_H2_gamgam'
-        plt.plot(obs[free], np.array(obs[process])/obs_nofree[process][0])
+                        free = angle
+                        pathFree = glob.glob(os.path.join(path, free, 'output_*'))[0]
+                        obs = TRSM.observables(pathFree, 'bb', 'gamgam', free)
+                        process = 'x_H3_H1_bb_H2_gamgam'
+                        ax.plot(obs[free], np.array(obs[process])/obs_nofree[process][0], alpha=0.5)
 
-    plt.savefig('test.pdf')
-    plt.close()
+                plt.savefig(os.path.join(pathPlots, f'{BPX}_{regionX}_{angle}_{process}.pdf'))
+                plt.close()
 
-    ## BP3
+    # vevs
 
-    # region 1
-
-    # region 2
