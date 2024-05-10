97979f9dcc96dd5014577cc1c9bdf87b0fdc0294
Irtiza Hayder Iram Haque <ihaque@lxplus903.cern.ch>
2024-05-10 14:56:03 +0200

    Some further changes to the oneD_param1D_plot.py, looks more similar to the plots in the thesis

--- parameter1D/oneD_param1D_plot.py
+++ parameter1D/oneD_param1D_plot.py
@@ -20,7 +20,19 @@ if __name__ == '__main__':
     pathOutputParent = '/eos/user/i/ihaque/parameter1DPlots' 
     
     # path to plots
-    pathPlots = os.path.join(pathRepo, 'plots')
+    pathPlots = os.path.join(pathRepo, 'parameter1D', 'plots')
+
+    processLatex = {'x_H3_H1_bb_H2_gamgam': 'gg\\to h_{1}(\gamma\gamma)h_{2}(b\\bar{b})',
+                    'x_H3_H1_gamgam_H2_bb': 'gg\\to h_{1}(b\\bar{b})h_{2}(\gamma\gamma)'}
+
+    regionLatex = {'region1': 'R1',
+                   'region2': 'R2'}
+
+    freeLatex = {'thetahS': '\\theta_{hS}',
+                 'thetahX': '\\theta_{hX}',
+                 'thetaSX': '\\theta_{SX}',
+                 'vs': 'v_{s}',
+                 'vx': 'v_{x}'}
     
     # angles
 
@@ -46,7 +58,11 @@ if __name__ == '__main__':
                         pathFree = glob.glob(os.path.join(path, free, 'output_*'))[0]
                         obs = TRSM.observables(pathFree, 'bb', 'gamgam', free)
                         process = 'x_H3_H1_bb_H2_gamgam'
-                        ax.plot(obs[free], np.array(obs[process])/obs_nofree[process][0], alpha=0.5)
+                        ax.plot(obs[free], np.array(obs[process])/obs_nofree[process][0], color='C0', alpha=0.5)
+                        # ax.legend(title=f'{BPX} $\sqrt{{s}}=13$\n')
+                        ax.set_title(f'{BPX} {regionLatex[regionX]}: $\sigma_{{{processLatex[process]}}}/\sigma^{{fixed}}_{processLatex[process]}$')
+                        ax.set_xlabel(f'${freeLatex[angle]}$')
+                        ax.set_ylabel(f'$\sigma_{processLatex[process]}/\sigma^{{fixed}}_{processLatex[process]}$')
 
                 plt.savefig(os.path.join(pathPlots, f'{BPX}_{regionX}_{angle}_{process}.pdf'))
                 plt.close()
