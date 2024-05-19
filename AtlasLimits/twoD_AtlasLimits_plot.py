9c77a9e7ef5af86dc96c11c85da98cf32f18d445
Irtiza Hayder Iram Haque <ihaque@lxplus927.cern.ch>
2024-05-19 14:34:17 +0200

    twoD_AtlasLimits_plot.py is pretty much finished, considering to implement imshow instead of scatter

--- AtlasLimits/twoD_AtlasLimits_plot.py
+++ AtlasLimits/twoD_AtlasLimits_plot.py
@@ -3,7 +3,6 @@ import os
 
 import pandas
 import numpy as np
-import scipy.interpolate
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.patheffects as pe
@@ -11,6 +10,31 @@ import matplotlib.patches as mpatches
 import mplhep as hep
 
 from helpScannerS import twoDPlotter as twoDPlot
+
+
+def cutter(x, y, z, xlim, ylim):
+
+    xNewlist = []
+    yNewlist = []
+    zNewlist = []
+
+    tol = 10 ** (-6)
+
+    for i in range(len(x)):
+        if (xlim[0] < x[i] and x[i] < xlim[1] and 
+            ylim[0] < y[i] and y[i] < ylim[1]):
+            xNewlist.append(x[i])
+            yNewlist.append(y[i])
+            zNewlist.append(z[i])
+
+            if (abs(x[i] - xlim[0]) < tol and
+                abs(x[i] - xlim[1]) < tol and
+                abs(y[i] - ylim[0]) < tol and
+                abs(y[i] - ylim[1]) < tol):
+                raise Exception('Function cutter is not precise enough for this.')
+
+    return xNewlist, yNewlist, zNewlist
+
 
 if __name__ == '__main__':
 
@@ -66,26 +90,21 @@ if __name__ == '__main__':
     dataIds = [element for element in limits.index]
 
     # normalize the limits by the SM di-Higgs to bbgamgam cross section
-    norm = (31.05 * 10**(-3)) * 0.002637
+    # norm = (31.05 * 10**(-3)) * 0.002637
     norm = (31.02 * 10**(-3) * 0.0026)
+
+    fontsize = 12
+    rotation = 45
+    linewidth = 2.0
 
     ## low mass
 
     fig, ax = plt.subplots()
 
-    # x, y, z, xi, yi = twoDPlot.plotAuxVar2D(ms, mx, np.array(XS)/norm)
+    msLow, mxLow, XSnormLow = cutter(ms, mx, np.array(XS)/norm,
+                                     (0, 270), (160, 420))
 
-    msi, mxi = np.linspace(min(ms), max(ms), 100), np.linspace(min(mx), max(mx), 200)
-    msi, mxi = np.meshgrid(msi, mxi)
-
-    zi = scipy.interpolate.griddata((ms, mx), np.array(XS)/norm, (msi, mxi), method='linear')
-
-    print(zi)
-    print(f'nanmax: {np.nanmax(zi)}')
-
-    # im = ax.scatter(ms, mx, c=np.array(XS)/norm)
-    im = ax.imshow(zi, origin='lower',
-                   extent=[min(ms), max(ms), min(mx), max(mx)], aspect='auto')
+    im = ax.scatter(msLow, mxLow, c=XSnormLow)
 
     twoDPlot.plotAuxTitleAndBounds2D(r'',
                                      r'$M_{S}$ [GeV]', r'$M_{X}$ [GeV]',
@@ -93,10 +112,10 @@ if __name__ == '__main__':
                                      xlims=(0, 270), ylims=(160, 420),
                                      fig=fig, ax=ax, im=im)
 
-    for i in range(len(XS)):
-        ax.annotate('{:.1f}'.format(XS[i]/norm), (ms[i], mx[i]),
-                    textcoords='offset points', xytext=(-3,-2), fontsize=10, rotation=45, 
-                    path_effects=[pe.withStroke(linewidth=1.5, foreground='w')])
+    for i in range(len(XSnormLow)):
+        ax.annotate('{:.1f}'.format(XSnormLow[i]), (msLow[i], mxLow[i]),
+                    textcoords='offset points', xytext=(-3,-2), fontsize=fontsize, rotation=rotation, 
+                    path_effects=[pe.withStroke(linewidth=linewidth, foreground='w')])
 
     # BP2
     ax.add_patch(mpatches.Rectangle((1,126), 123, 874,
@@ -108,6 +127,17 @@ if __name__ == '__main__':
                  linewidth=1, edgecolor='b', facecolor='none',
                  hatch=r'//\\', alpha=0.25, zorder=0))
 
+    # subregion of the original image
+    x1, x2, y1, y2 = 86, 117, 215, 257
+    axins = ax.inset_axes([200, 250, 60, 60],
+                          transform=ax.transData,
+                          xlim=(x1, x2), ylim=(y1, y2),
+                          xticklabels=[], yticklabels=[])
+
+    axins.scatter(msLow, mxLow, c=XSnormLow)
+
+    ax.indicate_inset_zoom(axins, edgecolor="black")
+
     title = 'ATLAS $\sqrt{s}=13$ TeV\n$gg\\to X\\to S(b\\bar{b}) H(\gamma \gamma)$\n95% C.L observed limit\n\
 $ref=gg\\to h_{SM}h_{SM}\\to b\\bar{b}\gamma\gamma$'
     ax.legend(title=title,
@@ -116,6 +146,7 @@ $ref=gg\\to h_{SM}h_{SM}\\to b\\bar{b}\gamma\gamma$'
               mpatches.Patch(linewidth=0, fill=None, alpha=0.25, color='b', hatch=r'//\\', label='BP3'),
               ], loc='lower right', alignment='left')
 
+    plt.tight_layout()
     plt.savefig(os.path.join(pathPlots, 'AtlasLimits_lowmass.pdf'))
     plt.close()
 
@@ -124,17 +155,20 @@ $ref=gg\\to h_{SM}h_{SM}\\to b\\bar{b}\gamma\gamma$'
 
     fig, ax = plt.subplots()
 
-    im = ax.scatter(ms, mx, c=np.array(XS)/norm)
+    msMed, mxMed, XSnormMed = cutter(ms, mx, np.array(XS)/norm,
+                                     (0, 525), (420, 620))
+
+    im = ax.scatter(msMed, mxMed, c=XSnormMed)
     twoDPlot.plotAuxTitleAndBounds2D(r'',
                                      r'$M_{S}$ [GeV]', r'$M_{X}$ [GeV]',
                                      r'$\sigma(obs)/\sigma(ref)$',
-                                     xlims=(0, 525), ylims=(420, 620),
+                                     xlims=(15, 490), ylims=(420, 610),
                                      fig=fig, ax=ax, im=im)
 
-    for i in range(len(XS)):
-        ax.annotate('{:.1f}'.format(XS[i]/norm), (ms[i], mx[i]),
-                    textcoords='offset points', xytext=(-3,-2), fontsize=10, rotation=45, 
-                    path_effects=[pe.withStroke(linewidth=1.5, foreground='w')])
+    for i in range(len(XSnormMed)):
+        ax.annotate('{:.1f}'.format(XSnormMed[i]), (msMed[i], mxMed[i]),
+                    textcoords='offset points', xytext=(-3,-2), fontsize=fontsize, rotation=rotation, 
+                    path_effects=[pe.withStroke(linewidth=linewidth, foreground='w')])
 
     # BP2
     ax.add_patch(mpatches.Rectangle((1,126), 123, 874,
@@ -154,6 +188,7 @@ $ref=gg\\to h_{SM}h_{SM}\\to b\\bar{b}\gamma\gamma$'
               mpatches.Patch(linewidth=0, fill=None, alpha=0.25, color='b', hatch=r'//\\', label='BP3'),
               ], loc='lower right', alignment='left')
 
+    plt.tight_layout()
     plt.savefig(os.path.join(pathPlots, 'AtlasLimits_mediummass.pdf'))
     plt.close()
 
@@ -162,17 +197,20 @@ $ref=gg\\to h_{SM}h_{SM}\\to b\\bar{b}\gamma\gamma$'
 
     fig, ax = plt.subplots()
 
-    im = ax.scatter(ms, mx, c=np.array(XS)/norm)
+    msHigh, mxHigh, XSnormHigh = cutter(ms, mx, np.array(XS)/norm,
+                                        (0, 525), (620, 1200))
+
+    im = ax.scatter(msHigh, mxHigh, c=XSnormHigh)
     twoDPlot.plotAuxTitleAndBounds2D(r'',
                                      r'$M_{S}$ [GeV]', r'$M_{X}$ [GeV]',
                                      r'$\sigma(obs)/\sigma(ref)$',
                                      xlims=(0, 525), ylims=(620, 1200),
                                      fig=fig, ax=ax, im=im)
 
-    for i in range(len(XS)):
-        ax.annotate('{:.1f}'.format(XS[i]/norm), (ms[i], mx[i]),
-                    textcoords='offset points', xytext=(-3,-2), fontsize=10, rotation=45, 
-                    path_effects=[pe.withStroke(linewidth=1.5, foreground='w')])
+    for i in range(len(XSnormHigh)):
+        ax.annotate('{:.1f}'.format(XSnormHigh[i]), (msHigh[i], mxHigh[i]),
+                    textcoords='offset points', xytext=(-3,-2), fontsize=fontsize, rotation=rotation, 
+                    path_effects=[pe.withStroke(linewidth=linewidth, foreground='w')])
 
     # BP2
     ax.add_patch(mpatches.Rectangle((1,126), 123, 874,
@@ -192,5 +230,6 @@ $ref=gg\\to h_{SM}h_{SM}\\to b\\bar{b}\gamma\gamma$'
               mpatches.Patch(linewidth=0, fill=None, alpha=0.25, color='b', hatch=r'//\\', label='BP3'),
               ], loc='upper left', alignment='left')
 
+    plt.tight_layout()
     plt.savefig(os.path.join(pathPlots, 'AtlasLimits_highmass.pdf'))
     plt.close()
